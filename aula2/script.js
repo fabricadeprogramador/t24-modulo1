37910b472e77d6d81a3cc1162b279d1585959585
Alison Moura <alison.oghino@gmail.com>
2019-03-09 00:11:25 -0400

    Fazendo alerta sucesso/erro

--- aula2/script.js
+++ aula2/script.js
@@ -5,13 +5,20 @@ function logar(){
     let senha = document.getElementById("usuario").value
 
     // Exibe alerta
-    exibirAlerta("Seu nome é: " + usuario + " e senha: " + senha)
+    if(usuario != "" && senha != "") {
+        exibirAlerta("sucesso", "Seu nome é: " + usuario + " e senha: " + senha)
+    }else {
+        exibirAlerta("erro", "Login ou senha inválido")
+    }
 }
 
-function exibirAlerta(texto) {
+function exibirAlerta(classe, texto) {
     let span = document.querySelector(".alerta span")
     span.innerHTML = texto
     let alerta = document.querySelector(".alerta")
+    alerta.classList.remove("erro")
+    alerta.classList.remove("sucesso")
+    alerta.classList.add(classe)
     alerta.style.display = "block"
 }
 
