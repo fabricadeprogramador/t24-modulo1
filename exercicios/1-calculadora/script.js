5496bc6e90cd0ce768fffa81a7d8f93c6eb7e1d6
Alison Moura <alison.oghino@gmail.com>
2019-03-13 15:23:38 -0400

    Criando exercício da calculadora

---
+++ exercicios/1-calculadora/script.js
@@ -0,0 +1,27 @@
+function somar(){
+    let n1 = Number.parseInt(document.getElementById("numero1").value)
+    let n2 = Number.parseInt(document.getElementById("numero2").value)
+
+    alert(n1+n2)
+}
+
+function subtrair(){
+    let n1 = Number.parseInt(document.getElementById("numero1").value)
+    let n2 = Number.parseInt(document.getElementById("numero2").value)
+
+    alert(n1-n2)
+}
+
+function multiplicar(){
+    let n1 = Number.parseInt(document.getElementById("numero1").value)
+    let n2 = Number.parseInt(document.getElementById("numero2").value)
+
+    alert(n1*n2)
+}
+
+function dividir(){
+    let n1 = Number.parseInt(document.getElementById("numero1").value)
+    let n2 = Number.parseInt(document.getElementById("numero2").value)
+
+    alert(n1/n2)
+}
