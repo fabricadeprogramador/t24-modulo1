2c04674ae9fc6a7b9b204702157890bdca2e8ad0
Alison Moura <alison.oghino@gmail.com>
2019-03-22 20:08:41 -0400

    Semáforo v5 - CSS+ JS+

--- aula3/semaforo/semaforo.js
+++ aula3/semaforo/semaforo.js
@@ -1,5 +1,6 @@
 
 function verificar() {
+    
     let selecionado = document.querySelector("input[type=radio]:checked")
     let sinal = ""
 
@@ -9,10 +10,13 @@ function verificar() {
     
     if(sinal == "vermelho") {
         alert("Pare!")
+        document.getElementById("semaforo").src = "vermelho.svg"
     } else if(sinal == "amarelo"){
         alert("Corre!")
+        document.getElementById("semaforo").src = "amarelo.svg"
     } else if(sinal == "verde"){
         alert("De boa!")
+        document.getElementById("semaforo").src = "verde.svg"
     } else {
         alert("Aí fica difícil! Seleciona algo aí")
     }
