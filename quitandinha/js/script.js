c2df7ac756133733142f843ace1af3f200b206ad
Alison Moura <alison.oghino@gmail.com>
2019-05-04 00:24:08 -0400

    Fazendo listagem e remoção de item da cesta

--- quitandinha/js/script.js
+++ quitandinha/js/script.js
@@ -3,8 +3,6 @@ let quantidadeCesta
 
 function adicionarCesta(produto) {
     cesta.push(produto)
-
-    alert("Sua cesta contém: " + cesta.toString())
     localStorage.setItem("cesta", JSON.stringify(cesta))
     carregarCesta()
 }
@@ -14,6 +12,36 @@ function carregarCesta() {
     quantidadeCesta = cesta.length
     // atualizar número na tela
     // pegar o span
-    document.getElementById("quantidadeCesta").innerHTML = quantidadeCesta
+    let quantidadeSpan = document.getElementById("quantidadeCesta")
+    if (quantidadeSpan) document.getElementById("quantidadeCesta").innerHTML = quantidadeCesta
     // atualizar valor do span
+}
+
+function listarCesta() {
+    carregarCesta()
+    let listaCesta = document.getElementById("listaCesta")
+    listaCesta.innerHTML = ""
+    for (let i = 0; i < cesta.length; i++) {
+        // let div = document.createElement('div')
+        // div.innerHTML = cesta[i]
+        // listaCesta.appendChild(div)
+        var template = document.createElement('template')
+        template.innerHTML = 
+        `<div class="item-cesta" id="item-${i}">
+            <span>
+                ${cesta[i]}
+            </span>
+            <button onclick="removerItemCesta(${i})" class="btn remover" title="Remover item da cesta">
+                Remover
+            </button>
+        </div>`
+        listaCesta.appendChild(template.content.childNodes[0])
+    }
+}
+
+function removerItemCesta(index) {
+    cesta.splice(index, 1);
+    localStorage.setItem("cesta", JSON.stringify(cesta))
+    document.getElementById(`item-${index}`).style.animation = "remove-item-cesta 1s forwards";
+    setTimeout(listarCesta, 1000)
 }
