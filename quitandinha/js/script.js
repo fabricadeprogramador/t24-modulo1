25dadd59fcddacdbb6bf9b7d579981262de5d431
Alison Moura <alison.oghino@gmail.com>
2019-05-11 14:05:27 -0400

    Aula do dia 11/05

--- quitandinha/js/script.js
+++ quitandinha/js/script.js
@@ -1,5 +1,4 @@
 let cesta = []
-let quantidadeCesta
 
 function adicionarCesta(produto) {
     cesta.push(produto)
@@ -11,18 +10,31 @@ function adicionarCesta(produto) {
 
 function carregarCesta() {
     cesta = JSON.parse(localStorage.getItem("cesta"))
-    quantidadeCesta = cesta.length
     // atualizar número na tela
     // pegar o span
     // atualizar valor do span
     if (document.getElementById("quantidadeCesta") != null) {
-        document.getElementById("quantidadeCesta").innerHTML = quantidadeCesta
+        document.getElementById("quantidadeCesta").innerHTML = cesta.length
     }
 }
 
 function listarCesta() {
     carregarCesta()
+    let lista = document.getElementById("listaCesta")
     for (let i = 0; i < cesta.length; i++) {
-        alert(cesta[i])
+
+        let template = document.createElement("template")
+        template.innerHTML = 
+        `<div class="item-cesta" id="item-${i}">
+            <span>${cesta[i]}</span>
+            <button class="btn remover" onclick="remover(${i})">Remover</button>
+        </div>
+        `
+
+        lista.appendChild(template.content.childNodes[0])
     }
+}
+
+function remover(index) {
+    document.getElementById(`item-${index}`).style.animation = "remove-cesta 2s"
 }
